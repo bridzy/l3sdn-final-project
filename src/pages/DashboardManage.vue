d8cfa2362d2a2098e8e965ebc76267b856c525c9
Amani yahia_bey <71192549+Amani05@users.noreply.github.com>
2024-04-14 20:21:33 +0200

    Update DashboardManage.vue

--- src/pages/DashboardManage.vue
+++ src/pages/DashboardManage.vue
@@ -1,11 +1,42 @@
 <template>
-  <q-page>
-    <h1>Tableau de bord Managé</h1>
+  <q-page class="q-pa-md">
+    <div>
+      <div class="text-h6">Votre Entretien</div>
+      <div v-if="managedInfo && managedInfo.entretien">
+        <p>Date d'entretien: {{ managedInfo.entretien.date }}</p>
+        <p>Manager: {{ managerName }}</p>
+      </div>
+      <div v-else>
+        <p>Aucun entretien planifié.</p>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'DashboardManage'
+  data() {
+    return {
+      managedInfo: null,
+      managerName: ''
+    }
+  },
+  methods: {
+    findManagerById(managerId) {
+      // Implémentez cette fonction pour rechercher les informations du manager par ID
+      const manager = window.userActions.getUserById(managerId)
+      return manager ? manager.username : 'Inconnu'
+    },
+    loadManagedInfo() {
+      // Implémentez cette fonction pour charger les informations du managé connecté
+      this.managedInfo = window.userActions.getCurrentManaged() // Exemple, ajustez selon votre logique d'authentification
+      if (this.managedInfo && this.managedInfo.entretien) {
+        this.managerName = this.findManagerById(this.managedInfo.entretien.managerId)
+      }
+    }
+  },
+  mounted() {
+    this.loadManagedInfo()
+  }
 }
 </script>
